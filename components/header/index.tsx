4629dc0e529a15ff8dfcd1d1ff4a89f0e422fb9a
WasifAli22 <wasifaliwasif22@gmail.com>
2023-04-21 20:24:24 +0500

    anchor tag replaced with Link Component

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
-
+import Link from 'next/link'
 export default function Header() {
     return (
         <header className="px-4 bg-[#232536]">
             <div className="container flex justify-between h-16 mx-auto">
-                <a rel="noopener noreferrer" href="/" aria-label="Back to homepage" className="flex text-2xl text-white font-bold items-center p-2">
+                <Link rel="noopener noreferrer" href="/" aria-label="Back to homepage" className="flex text-2xl text-white font-bold items-center p-2">
                     Wasif
-                </a>
+                </Link>
                 <ul className="items-center text-white hidden space-x-3 md:flex">
                     <li className="flex">
-                        <a rel="noopener noreferrer" href="/" className="flex font-light items-center px-4 -mb-1  dark:border-transparent">Home</a>
+                        <Link rel="noopener noreferrer" href="/" className="flex font-light items-center px-4 -mb-1  dark:border-transparent">Home</Link>
                     </li>
                     <li className="flex">
-                        <a rel="noopener noreferrer" target='_blank' href="https://portfoliowebsites-git-main-wasifali22.vercel.app" className="flex font-light items-center px-4 -mb-1  dark:border-transparent dark:text-violet-400 dark:border-violet-400">About Us</a>
+                        <Link rel="noopener noreferrer" target='_blank' href="https://portfoliowebsites-git-main-wasifali22.vercel.app" className="flex font-light items-center px-4 -mb-1  dark:border-transparent dark:text-violet-400 dark:border-violet-400">About Us</Link>
                     </li>
                     <li className="flex">
-                        <a rel="noopener noreferrer" href="/contact-us" className="flex font-light items-center px-4 -mb-1  dark:border-transparent">Contact Us</a>
+                        <Link rel="noopener noreferrer" href="/contact-us" className="flex font-light items-center px-4 -mb-1  dark:border-transparent">Contact Us</Link>
                     </li>
                     <li className="flex">
                         <a rel="noopener noreferrer" href="#" className="dark:bg-gray-100 bg-white dark:text-gray-800 text-[#232536] py-2 px-4 font-medium text-xl dark:border-transparent">Subscribe</a>
